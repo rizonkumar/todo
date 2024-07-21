04536d76a0e98a78b5e6753c7432ff30f705a7e8
rizon.rahi <rizon.rahi@ugamsolutions.com>
2024-07-21 11:43:36 +0530

    todo app inital commit

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+
+const app = express();
+
+app.use(express.json());
+
+
+
+app.post("/todo", function(req, res) {
+
+})
+
+app.get("/todos", function(req, res) {
+
+})
+
+app.post("/completed", function(req, res) {
+
+})
+
+app.listen(3000)
---
+++ backend/types.js
@@ -0,0 +1,17 @@
+const zod = require("zod");
+
+const createToDo = zod.object({
+    title: zod.string(),
+    description: zod.string()
+})
+
+
+const updateToDo = zod.object({
+    id: zod.string(),
+})
+
+
+module.exports = {
+    createToDo: createToDo,
+    updateToDo: updateToDo
+}

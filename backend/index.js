3711241795a6d9a1884f5487c4fcea12713aea45
rizon.rahi <rizon.rahi@ugamsolutions.com>
2024-07-21 11:47:58 +0530

    Feat: Basic Route for the todo & completed

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,32 @@
 const express = require("express");
-
+const { createToDo } = require("./types");
 
 const app = express();
 
 app.use(express.json());
 
+app.post("/todo", function (req, res) {
+  const createPayload = req.body;
+  const parsedPayload = createToDo.safeParse(createPayload);
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "You sent the wrong inputs",
+    });
+    return;
+  }
+});
 
+app.get("/todos", function (req, res) {});
 
-app.post("/todo", function(req, res) {
+app.put("/completed", function (req, res) {
+  const updatePayload = req.body;
+  const parsedPayload = updatePayload.safeParse(updatePayload);
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "You sent the wrong inputs",
+    });
+    return;
+  }
+});
 
-})
-
-app.get("/todos", function(req, res) {
-
-})
-
-app.post("/completed", function(req, res) {
-
-})
-
-app.listen(3000)
+app.listen(3000);
